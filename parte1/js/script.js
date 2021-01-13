d3411d2eb5feb7433b5ab207a31578fd6d09fb9b
Pedro Henrique <pedro.progsantos@gmail.com>
2021-01-13 16:45:27 -0300

    Finalizado

--- parte1/js/script.js
+++ parte1/js/script.js
@@ -1,1 +1,27 @@
 /* Coloque o JS neste arquivo */
+$(document).ready(function(){
+    $('.banner-list').slick({
+        dots: false,
+        arrows: true,
+    });
+
+    $('.nav-icon').on('click', function(){
+        if($('.navigation').hasClass('active')){
+            $('.navigation').removeClass('active');
+        }else{
+            $('.navigation').addClass('active')
+        }
+    });
+
+    $(window).on('scroll', function(e){
+        if($(window).scrollTop() > 200){
+            $('.bottom-links').addClass('active');
+        }else{
+            $('.bottom-links').removeClass('active');
+        }
+    });
+
+    $('.up').on('click', function(){
+        $('html, body').animate({scrollTop:0}, 'slow'); //slow, medium, fast
+    });
+});
